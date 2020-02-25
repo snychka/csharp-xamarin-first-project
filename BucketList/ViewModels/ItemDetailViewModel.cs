f94e43284bafc1a65b9dc201e78ca6a12ba9cdcb
Jeff Hopper <jeff@hoppertech.net>
2020-02-24 20:23:08 -0700

    Base Master

---
+++ BucketList/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BucketList.Models;
+
+namespace BucketList.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
