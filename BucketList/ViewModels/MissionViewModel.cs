5657b987d70aea2abf966ad341812fb09cb5bd8e
Jeff Hopper <jeff@hoppertech.net>
2020-04-14 11:54:32 -0700

    Added MissionPage and supporting files

---
+++ BucketList/ViewModels/MissionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BucketList.ViewModels
+{
+    public class MissionViewModel : BaseViewModel
+    {
+        string missionStatement;
+        public string MissionStatement
+        {
+            get { return missionStatement; }
+            set { SetProperty(ref missionStatement, value); }
+        }
+
+        public Command LoadMissionCommand { get; set; }
+        public Command SaveMissionCommand { get; set; }
+
+        public MissionViewModel()
+        {
+            Title = "Mission Statement";
+            LoadMissionCommand = new Command(async () => missionStatement = await DataStore.GetMission());
+            SaveMissionCommand = new Command(async () => await DataStore.UpdateMission(missionStatement));
+            
+        }
+    }
+}
