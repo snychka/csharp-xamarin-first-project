4ebdc9562626e5889d12c3e614ade7ca1f4c9aa3
Jeff Hopper <jeff@hoppertech.net>
2020-04-23 11:54:56 -0700

    Apply module 3 task changes

--- BucketList/Models/Item.cs
+++ BucketList/Models/Item.cs
@@ -7,5 +7,6 @@ namespace BucketList.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
